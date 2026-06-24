8a6fce7477d88ed87738030beb0c9b3c59602f78
Ayush yadav <ayush1337yadav@gmail.com>
2026-06-24 09:17:34 +0530

    updated till mlflow autolog

---
+++ src/autolog.py
@@ -0,0 +1,65 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# to log all the artifacts and params on remote server
+import dagshub
+dagshub.init(repo_owner='coderay3000', repo_name='YT-MLOPS-Experiments-with-mlflow', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/coderay3000/YT-MLOPS-Experiments-with-mlflow.mlflow")
+
+
+# Load Wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# Define the params for RF model
+max_depth = 8
+n_estimators = 5
+
+
+# mention your experiments below
+mlflow.autolog()
+mlflow.set_experiment('YT-MlOPS-Exp2') # it also creates new experiment
+
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators,random_state=42)
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+    accuracy=accuracy_score(y_test,y_pred)
+
+   
+    #creating a confusion matrix plot
+    cm = confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm,annot=True,cmap='Blues',xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+
+    #save plot
+    plt.savefig("Confusion-matrix.png")
+
+    #log artifacts using mlflow
+   
+    mlflow.log_artifact(__file__) # autolog just do not log file this need to explicitly mentioned
+    
+    # tags
+    mlflow.set_tags({"Author":'Vikash',"Project":"Wine Classification"}) #autolog just do not log tags this need to explicitly mentioned
+
+    
+
+
+    print(accuracy)
+
